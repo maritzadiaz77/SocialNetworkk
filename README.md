5332a9cbea506583e141e08a0d03a45fae9ea331
Maritza Diaz <maritzadiaz77@gmail.com>
2025-01-22 17:37:15 -0800

    added package.json file and modified js files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SocialNetworkk
+
+# Description
+
+# Installation
+
+This is an API for a social network web application where users can share their thoughts, react to friends' thoughts, and create a friend list. It uses `Express.js` for routing, a `MongoDB` database, the `Mongoose` ODM, and `Moment.js` to format timestamps. The seed data is created using `Insomnia`.
+
+# Credits
---
+++ models/User.js
@@ -0,0 +1,48 @@
+// imports
+const { Schema, model } = require("mongoose");
+
+// User schema
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, "Must match an email address!"],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    });
+
+// increases friend count in User model object when friends are added by a user
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
+
+// creates User model with userSchema
+const User = model("user", userSchema);
+
+// exports
+module.exports = User;
